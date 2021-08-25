45b9251060b83ad4d6b90b0a0e7a1b388185d726
mitetodb <65173088+mitetodb@users.noreply.github.com>
2021-08-25 23:20:39 +0200

    create routing with nested modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,36 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
-import { ProfileComponent } from './user/profile/profile.component';
-import { CatalogComponent } from './recipes/catalog/catalog.component';
-import { CreateComponent } from './recipes/create/create.component';
-import { DetailsComponent } from './recipes/details/details.component';
-import { EditComponent } from './recipes/edit/edit.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
+import { RecipesModule } from './recipes/recipes.module';
+import { ContentService } from './content.service';
+import { UserService } from './user/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    LoginComponent,
-    RegisterComponent,
-    ProfileComponent,
-    CatalogComponent,
-    CreateComponent,
-    DetailsComponent,
-    EditComponent,
     IngredientsComponent,
     NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    UserModule,
+    RecipesModule
   ],
-  providers: [],
+  providers: [
+    ContentService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
