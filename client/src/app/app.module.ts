1f76fcdae123bbbbd6c1f864c6642abe62a502fe
mitetodb <65173088+mitetodb@users.noreply.github.com>
2021-08-25 22:46:14 +0200

    create site structure components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { CatalogComponent } from './recipes/catalog/catalog.component';
+import { CreateComponent } from './recipes/create/create.component';
+import { DetailsComponent } from './recipes/details/details.component';
+import { EditComponent } from './recipes/edit/edit.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    CatalogComponent,
+    CreateComponent,
+    DetailsComponent,
+    EditComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule,
